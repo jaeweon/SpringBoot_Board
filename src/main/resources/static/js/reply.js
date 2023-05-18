4ac314f91acaac73ebaa3e62676ae010ba1d645d
jaeweon <fhfkfk789@naver.com>
2023-05-19 01:31:37 +0900

    insert, select, update, delete test finished

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -16,5 +16,62 @@ let replyService = (function (){
         });
     }
 
-    return {add : add};
+        function getList(param, callback){
+            let boardNumber = param.boardNumber;
+            let page = param.page || 1;
+            console.log("param..........");
+            console.log(boardNumber + page);
+            $.getJSON("/reply/list/" + boardNumber + "/" + page, function (list){
+                if(callback){
+                    callback(list);
+                }
+            });
+        }
+
+        function read(replyNumber, callback){
+            $.get("/reply/" + replyNumber, function (reply){
+                if (callback){
+                    callback(reply);
+                }
+            });
+        }
+
+    function remove(replyNumber , callback){
+        console.log("remove reply..........");
+        console.log(replyNumber);
+        $.ajax({
+            url: "/reply/"  + replyNumber,
+            type: "delete",
+            success: function (reply){
+                if(callback){
+                    callback(reply);
+                }
+            }
+        });
+    }
+
+    function modify(reply, callback){
+        $.ajax({
+            url: "/reply/" + reply.replyNumber,
+            type: "post",
+            data: JSON.stringify(reply),
+            contentType: "application/json",
+            success: function (result){
+                if(callback){
+                    callback(result);
+                }
+            }
+        });
+    }
+
+
+    return {add : add, getList : getList, read : read, remove : remove, modify : modify};
 })();
+
+
+
+
+
+
+
+
